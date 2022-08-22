234f922aec8336d0f7b07760e211e7399743727e
Mikolaj <mikolaj.studencki@OKE.pl>
2022-08-22 16:26:53 +0200

    hedderV1

--- src/components/layout/Navbar/NavBar.js
+++ src/components/layout/Navbar/NavBar.js
@@ -4,7 +4,15 @@ import "./Navbar.css";
 function Navbar() {
   return (
     <div className="Navbar">
-    </div>
+    <img src={require("../../../images/poke.png")} alt="poke" width="40%" height = "90%"/>
+      <div>
+        <h3> By Mikołaj Studencki and Piotr Zalewski </h3>
+      </div>
+        <div classname= "pages">
+          <a href="/">PokeDex</a>
+          <a href="/ulubione">Ulubione</a>
+        </div>
+      </div>
   );
 }
 
