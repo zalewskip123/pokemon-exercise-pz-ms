7eba3cbbb200a852eea41dfe5c23a09fc6078731
Piotr <zalewskipiotr050@gmail.com>
2022-08-22 13:28:43 +0200

    Add Main and Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 
+import "./App.css";
+
+import Navbar from "./components/layout/Navbar/NavBar";
+import Main from "./components/layout/Main/Main";
+
 function App() {
   return (
     <div className="App">
-      <p>Test</p>
+      <Navbar />
+      <Main />
     </div>
   );
 }
---
+++ src/components/layout/Main/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Main.css";
+
+function Main() {
+  return (
+    <main className="Main">
+      <div className="sidebar" />
+      <div className="maindiv">
+      </div>
+      <div className="sidebar" />
+    </main>
+  );
+}
+
+export default Main;
