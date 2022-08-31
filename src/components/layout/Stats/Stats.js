25095fbe98e3a00e7e3d440a7b5145bde2ee1782
Mikolaj <mikolaj.studencki@OKE.pl>
2022-08-31 11:20:08 +0200

    DoPoprawy

--- src/components/layout/Stats/Stats.js
+++ src/components/layout/Stats/Stats.js
@@ -38,8 +38,10 @@ const Stats = () => {
   }
 
   const [result, setResult] = useState(undefined);
+  const [result2, setResult2] = useState(undefined);
 
   const URL = `https://pokeapi.co/api/v2/pokemon/${name}`;
+  const URL2 = `https://pokeapi.co/api/v2/pokemon-species/${name}`;
 
   useEffect(() => {
     fetch(URL)
@@ -59,6 +61,24 @@ const Stats = () => {
         })
 }, [URL])
 
+useEffect(() => {
+  fetch(URL2)
+      .then(response2 => {
+          if(response2.ok) {
+              return response2
+          }
+          throw Error("Nie udało się")
+      })
+      .then(response2 => response2.json())
+      .then(data2 => {
+        setResult2(data2);
+        console.log(data2);
+      })
+      .catch(err => {
+          console.log(err);
+      })
+}, [URL2])
+
 const ImageURL = result && result.sprites.front_default;
 const HPStat = result && result.stats[0].base_stat;
 const ATStat = result && result.stats[1].base_stat;
@@ -66,6 +86,8 @@ const DEFStat = result && result.stats[2].base_stat;
 const SPEStat = result && result.stats[5].base_stat;
 const SATStat = result && result.stats[3].base_stat;
 const SDEFStat = result && result.stats[4].base_stat;
+
+const FlaworText = result2 && result2.flavor_text_entries[6].flavor_text.replace('\f', ' ');
 
 const Bar = (Stat) => {
   return (
@@ -93,31 +115,36 @@ const Bar = (Stat) => {
                 </h3>
             ))}
             <div>
-              <img src={ImageURL !== null ? ImageURL : require("../../../images/ball.png")} alt="Pokemonimage"/>
+              <div className="center">
+                <div className="image">
+                  <img src={ImageURL !== null ? ImageURL : require("../../../images/ball.png")} alt="Pokemonimage"/>
+                </div>
+                <p className="flavorText"> {FlaworText && FlaworText.replace('é', 'E')}</p>
+              </div>
               <div className="stats">
                 <h2>Stats: </h2>
                 <div>
-                  <p>HP:</p>
+                  <p className="statsP">HP:</p>
                   {Bar(HPStat)}
                 </div>
                 <div>
-                  <p>Attack:</p>
+                  <p className="statsP">Attack:</p>
                   {Bar(ATStat)}
                 </div>
                 <div>
-                  <p>Defense:</p>
+                  <p className="statsP">Defense:</p>
                   {Bar(DEFStat)}
                 </div>
                 <div>
-                  <p>Speed:</p>
+                  <p className="statsP">Speed:</p>
                   {Bar(SPEStat)}
                 </div>
                 <div>
-                  <p>Special Attack:</p>
+                  <p className="statsP">Special Attack:</p>
                   {Bar(SATStat)}
                 </div>
                 <div>
-                  <p>Special Defense:</p>
+                  <p className="statsP">Special Defense:</p>
                   {Bar(SDEFStat)}
                 </div>
               </div>
